48122cc7ea5b1e0036ef3e7c5e6064f0bbfd8388
dirk.he <dirk.he@redteamobile.com>
2021-10-28 14:37:59 +0800

    add nested page

--- js/authA.be6b609a.js
+++ js/authA.be6b609a.js
@@ -1,8 +1,8 @@
 /*!
  * design: redteamobile.com
- * hash: 579bdef986bf6a85cfc5
+ * hash: 0f139d21dc904defb57a
  * chunkhash: be6b609a961b431a2ac2
  * name: authA
- * _: 2021-9-28 17:03:53
+ * _: 2021-10-28 14:35:25
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[4],{a88b:function(e,a,t){"use strict";t.r(a);var n=function(){var e=this,a=e.$createElement,t=e._self._c||a;return t("div",[e._v("admin才能看到")])},c=[],r=t("9ab4"),s=t("60a3");let d=class extends s["c"]{created(){}};d=Object(r["__decorate"])([Object(s["a"])({name:"Index"})],d);var l=d,b=l,i=t("2877"),o=Object(i["a"])(b,n,c,!1,null,"07b48fc2",null);a["default"]=o.exports}}]);
---
+++ js/nested3.bed27db0.js
@@ -0,0 +1,8 @@
+/*!
+ * design: redteamobile.com
+ * hash: 0f139d21dc904defb57a
+ * chunkhash: bed27db0f86237acdb26
+ * name: nested3
+ * _: 2021-10-28 14:35:25
+ */
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[27],{aeb4:function(e,t,a){"use strict";a.r(t);var c=function(){var e=this,t=e.$createElement,a=e._self._c||t;return a("div",[a("router-view")],1)},n=[],r=a("9ab4"),s=a("60a3");let l=class extends s["c"]{};l=Object(r["__decorate"])([s["a"]],l);var o=l,u=o,i=a("2877"),w=Object(i["a"])(u,c,n,!1,null,"5048601c",null);t["default"]=w.exports}}]);
